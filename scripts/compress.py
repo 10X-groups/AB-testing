ad4f12ed5b990f55b68bfd655966629f520672eb
fish <fisseha.137@gmail.com>
2022-08-30 17:08:26 +0300

    added a logger

--- scripts/compress.py
+++ scripts/compress.py
@@ -7,6 +7,11 @@ import warnings
 import pandas as pd
 warnings.filterwarnings('ignore')
 
+# setting up logger
+from logger_importer import setup_logger
+logger= setup_logger('logs/compress_root.log')
+logger.info(f'created a data compressor logger for compress.py file.')
+
 # set up paths and helper scripts
 sys.path.append('.')
 sys.path.insert(1, '../scripts/')
@@ -17,12 +22,14 @@ version = 'v1'
 
 # data path using dvc api
 data_url = dvc.api.get_url(path = path, rev = version)
+logger.info(f'obtained the data url: {data_url}')
 
 # reading the data
 missing_values = ["n/a", "na", "undefined", '?', 'NA', 'undefined']
 df = pd.read_csv(data_url, na_values=missing_values)
-
+logger.info(f'obtained the data {df.shape} from the data url: {data_url}')
 
 # compress and save the original data set
 df.to_csv(path + '.bz2', index=False)
-print('file compressed and saved successfully.')
+logger.info(f'file compressed and saved successfully to {path + ".bz2"}')
+print(f'file compressed and saved successfully to {path + ".bz2"}')
